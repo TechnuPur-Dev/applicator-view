4317ee239e4be687677812908f9a9600edca41f0
Muhammad Mudassar Hameed <mudassaransari324@gmail.com>
2025-01-13 11:00:37 +0500

    feat(schema): add custom names for applicator-grower relationships

---
+++ db/migrations/20250113060008_add_custom_names_for_applicator_grower_relationships/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "ApplcatorGrower" ADD COLUMN     "applicatorFirstName" TEXT,
+ADD COLUMN     "applicatorLastName" TEXT,
+ADD COLUMN     "growerFirstName" TEXT,
+ADD COLUMN     "growerLastName" TEXT;
