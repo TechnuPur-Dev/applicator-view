f43943e9c7efaec7d429504e5079c69ed8b21f17
Muhammad Mudassar Hameed <mudassaransari324@gmail.com>
2025-01-14 13:57:58 +0500

    fix: configure Vercel to use root package.json for dependencies, update pre-push husky hook and remove the eslintingnore file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,17 +4,23 @@ import jestPlugin from 'eslint-plugin-jest';
 import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
-    {
-        // Ignores files and directories not relevant for linting
-        ignores: ['**/build/**', '**/dist/**', 'coverage', 'docker'],
-    },
-    prettierConfig, // Disable ESLint rules that conflict with Prettier
-    eslint.configs.recommended, // Base recommended ESLint rules
-    ...tseslint.configs.strict, // Strict rules for catching potential bugs
-    ...tseslint.configs.stylistic, // Rules for consistent styling (optional)
-    {
-        // Jest plugin config for .spec.ts files
-        files: ['**/*.spec.ts'],
-        ...jestPlugin.configs['flat/recommended'],
-    }
+	{
+		// Ignores files and directories not relevant for linting
+		ignores: [
+			'**/build/**',
+			'**/dist/**',
+			'node_modules/',
+			'coverage',
+			'docker',
+		],
+	},
+	prettierConfig, // Disable ESLint rules that conflict with Prettier
+	eslint.configs.recommended, // Base recommended ESLint rules
+	...tseslint.configs.strict, // Strict rules for catching potential bugs
+	...tseslint.configs.stylistic, // Rules for consistent styling (optional)
+	{
+		// Jest plugin config for .spec.ts files
+		files: ['**/*.spec.ts'],
+		...jestPlugin.configs['flat/recommended'],
+	},
 );
