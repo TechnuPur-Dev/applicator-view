40259328768fc2b48cb71bb439df87411ba1cf35
Tehreem Iqbal <tehreemiqbal91@gmail.com>
2025-01-15 11:44:16 +0500

    feat(user): Create delete growers by applicator end point(/delete-grower/id/userId)

--- apps/user-service/src/modules/user/user-routes.ts
+++ apps/user-service/src/modules/user/user-routes.ts
@@ -8,11 +8,16 @@ const router: Router = express.Router();
 // Define routes
 router.route('/all-users').get(userController.getUserList);
 router.route('/all-growers').get(userController.getAllGrowers);
+router.route('/create-grower').post(userController.createGrower);
+
+// dynamic routes
 router.route('/:email').get(userController.getUserByEmail);
 router.route('/:id').get(userController.getUserById);
 router.route('/:id').patch(userController.updateUserById);
 router.route('/:id').delete(userController.deleteUser);
-router.route('/create-grower').post(userController.createGrower);
+router.route('/delete-grower/:id/:userId').delete(userController.deleteGrower);
+
+
 
 
 
