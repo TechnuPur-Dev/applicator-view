242f3c77c451c8130eecc219ca7a33858c8a57fb
Muhammad Mudassar Hameed <mudassaransari324@gmail.com>
2025-01-23 21:28:36 +0500

    feat(schema): added schema models for Job, FieldJob, JobProduct and JobApplicationFee

--- db/migrations/20250113060008_add_custom_names_for_applicator_grower_relationships/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "ApplcatorGrower" ADD COLUMN     "applicatorFirstName" TEXT,
-ADD COLUMN     "applicatorLastName" TEXT,
-ADD COLUMN     "growerFirstName" TEXT,
-ADD COLUMN     "growerLastName" TEXT;
