b3caae0f0bed88b50df088b38ce6b40809e8a475
Tehreem Iqbal <tehreemiqbal91@gmail.com>
2025-04-09 12:07:59 +0500

    update user-services modules for applicator-user

--- apps/user-service/src/modules/applicator-workers/applicator-workers-routes.ts
+++ apps/user-service/src/modules/applicator-workers/applicator-workers-routes.ts
@@ -5,48 +5,52 @@ import applicatorWorker from './applicator-workers-controller';
 import { verifyToken } from '../../../../../shared/middlewares/auth-middleware'; // Uncomment and add correct path for TypeScript support if needed
 import validateSchema from '../../../../../shared/middlewares/validation-middleware';
 import workerValidation from './applicator-workers-validation';
+import { normalizeApplicatorUser } from '../../../../../shared/middlewares/normalize-user-middleware';
 const router: Router = express.Router();
-
+router.use(verifyToken);
+router.use(normalizeApplicatorUser);
 router
 	.route('/create')
 	.post(
-		verifyToken,
+		// verifyToken,
 		validateSchema(workerValidation.workerCreateSchema),
 		applicatorWorker.createWorker,
 	);
-router.route('/get/all').get(verifyToken, applicatorWorker.getAllWorkers);
+router.route('/get/all').get(
+	// verifyToken,
+	applicatorWorker.getAllWorkers);
 router
 	.route('/get/by-Id/:id')
 	.get(
-		verifyToken,
+		// verifyToken,
 		validateSchema(workerValidation.paramsSchema),
 		applicatorWorker.getWorkerById,
 	);
 router
 	.route('/send/invite/:id')
 	.put(
-		verifyToken,
+		// verifyToken,
 		validateSchema(workerValidation.sendInviteSchema),
 		applicatorWorker.sendInviteToWorker,
 	);
 router
 	.route('/update/invite-status')
 	.put(
-		verifyToken,
+		// verifyToken,
 		validateSchema(workerValidation.updateInviteStatusSchema),
 		applicatorWorker.updateInviteStatus,
 	);
 router
 	.route('/update/:id')
 	.put(
-		verifyToken,
+		// verifyToken,
 		validateSchema(workerValidation.updateSchema),
 		applicatorWorker.updateWorker,
 	);
 router
 	.route('/delete/:id')
 	.delete(
-		verifyToken,
+		// verifyToken,
 		validateSchema(workerValidation.paramsSchema),
 		applicatorWorker.deleteWorker,
 	);
@@ -54,14 +58,18 @@ router
 router
 	.route('/search/by-email/:email')
 	.get(
-		verifyToken,
+		// verifyToken,
 		validateSchema(workerValidation.searchWorkerByEmail),
 		applicatorWorker.searchWorkerByEmail,
 	);
 router
 	.route('/get/all-applicators')
-	.get(verifyToken, applicatorWorker.getAllApplicators);
+	.get(
+		// verifyToken, 
+		applicatorWorker.getAllApplicators);
 router
 	.route('/pending-invites')
-	.get(verifyToken, applicatorWorker.getPendingInvites);
+	.get(
+		// verifyToken, 
+		applicatorWorker.getPendingInvites);
 export default router;
