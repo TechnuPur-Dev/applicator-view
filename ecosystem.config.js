ced69efbab299a8e2e7bbd241903ce7fe89f9642
Muhammad Mudassar Hameed <mudassaransari324@gmail.com>
2025-07-16 21:00:14 +0500

    fix: resolved deployment issues

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,38 @@
-export default {
+/* eslint-disable no-undef */
+module.exports = {
 	apps: [
 		{
 			name: 'user-service',
-			script: './apps/user-service/index.js', // or dist/main.js
+			script: 'node_modules/.bin/ts-node',
+			args: 'apps/user-service/index.ts',
+			cwd: '.', // root of the monorepo
+			interpreter: 'none',
+			env: {
+				NODE_ENV: 'production',
+				PORT: 3000,
+			},
 		},
 		{
 			name: 'job-service',
-			script: './apps/job-service/index.js',
+			script: 'node_modules/.bin/ts-node',
+			args: 'apps/job-service/index.ts',
+			cwd: '.',
+			interpreter: 'none',
+			env: {
+				NODE_ENV: 'production',
+				PORT: 3001,
+			},
 		},
 		{
-			name: 'grower-service',
-			script: './apps/grower-service/index.js',
+			name: 'admin-service',
+			script: 'node_modules/.bin/ts-node',
+			args: 'apps/admin-service/index.ts',
+			cwd: '.',
+			interpreter: 'none',
+			env: {
+				NODE_ENV: 'production',
+				PORT: 3002,
+			},
 		},
 	],
 };
